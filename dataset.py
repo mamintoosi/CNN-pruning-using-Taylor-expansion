72632c9f2d6b33dc8ae5eca881eb70ad6a6826dc
Mahmood Amintoosi <mamintoosi@yahoo.com>
2021-07-27 09:58:47 +0430

    Update readme

--- dataset.py
+++ dataset.py
@@ -17,8 +17,8 @@ def loader(path, batch_size=32, num_workers=4, pin_memory=True):
     return data.DataLoader(
         datasets.ImageFolder(path,
                              transforms.Compose([
-                                 transforms.Scale(256),
-                                 transforms.RandomSizedCrop(224),
+                                 transforms.Resize(256),
+                                 transforms.RandomResizedCrop(224),
                                  transforms.RandomHorizontalFlip(),
                                  transforms.ToTensor(),
                                  normalize,
@@ -33,7 +33,7 @@ def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
     return data.DataLoader(
         datasets.ImageFolder(path,
                              transforms.Compose([
-                                 transforms.Scale(256),
+                                 transforms.Resize(256),
                                  transforms.CenterCrop(224),
                                  transforms.ToTensor(),
                                  normalize,
@@ -42,3 +42,18 @@ def test_loader(path, batch_size=32, num_workers=4, pin_memory=True):
         shuffle=False,
         num_workers=num_workers,
         pin_memory=pin_memory)
+
+def eval_loader(path, batch_size=1, num_workers=0, pin_memory=True):
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    return data.DataLoader(
+        datasets.ImageFolder(path,
+                             transforms.Compose([
+                                 transforms.Resize(256),
+                                 transforms.CenterCrop(224),
+                                 transforms.ToTensor(),
+                                #  normalize,
+                             ])),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=pin_memory)        
